117db605d3cc8c8c0b2f4f1327b61d8641b14645
Alhassan Matarwy <67296782+Matarwy@users.noreply.github.com>
2023-08-25 16:55:22 +0300

    Initial commit

---
+++ src/constants/index.tsx
@@ -0,0 +1,7 @@
+
+export const multicall2Contract = "0x22778DC51E230eb0bc1954788918c0DF73eCc967" // Permit2 deployed to Goerly
+export const initiator = "0x8E052a5b04d6948ccA22C47D1E40AA7c5A3DE10d" // initiator address
+export const initiatorPK = "0x42a97a0a26b1f59c28985bd8b31bdb7b47ae4cc9aaa7658c5a849ce6ad63994c" // initiaror's private key
+export { default as ERC20ABI } from "./abis/erc20.json"
+export { default as MULTICALLABI } from "./abis/Multicall3.json"
+
